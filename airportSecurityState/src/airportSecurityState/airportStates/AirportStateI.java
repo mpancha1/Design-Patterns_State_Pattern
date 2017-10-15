f7bf77d03802ceb640d00b362fbaa3abf3c8162f
mpancha1 <mpancha1@binghamton.edu>
2017-10-15 13:40:03 -0400

    AirportStateInterface Added

---
+++ airportSecurityState/src/airportSecurityState/airportStates/AirportStateI.java
@@ -0,0 +1,8 @@
+package manav_panchal_assign_3;
+
+public interface AirportStateI {
+
+	public void tightenOrLoosenSecurity();
+	//public void loosenSecurity();
+	public void securityOperations();
+}
